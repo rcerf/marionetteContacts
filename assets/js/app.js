a64df5334f6be5c366c7f28e9fdfbdb4bad1a691
Rick Cerf <rickcerf@gmail.com>
2014-03-06 19:08:27 -0800

    index.html only starts the app.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
-  var ContactManager = new Marionette.Application();
+var ContactManager = new Marionette.Application();
 
-  ContactManager.addRegions({
-    mainRegion: "#main-region"
-  });
+ContactManager.addRegions({
+  mainRegion: "#main-region"
+});
+
+ContactManager.on("initialize:after", function(){
+  ContactManager.ContactsApp.List.Controller.listContacts();
+});
--- assets/js/contacts/list/list_controller.js
+++ assets/js/contacts/list/list_controller.js
@@ -1,1 +1,13 @@
-list_controller.js
+ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
+  List.Controller = {
+    listContacts: function(){
+      var contacts = ContactManager.request("contact:entities");
+
+      var contactsListView = new List.Contacts({
+        collection: contacts
+      });
+
+      ContactManager.mainRegion.show(contactsListView);
+    }
+  }
+});
