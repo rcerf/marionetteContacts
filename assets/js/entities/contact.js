e2db0670e0478ea012783e48cde2020fb9343661
Rick Cerf <rickcerf@gmail.com>
2014-03-06 17:23:47 -0800

    created List view module

--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -1,5 +1,5 @@
 ContactManager.module("Entities", function(Entities, ContactManager, Backbone, Marionette, $, _){
-  ContactManager.Contact = Backbone.Model.extend({
+  Entities.Contact = Backbone.Model.extend({
     defaults: {
       firstName: "",
       lastName: "",
@@ -7,8 +7,8 @@ ContactManager.module("Entities", function(Entities, ContactManager, Backbone, M
     }
   });
 
-  ContactManager.ContactCollection = Backbone.Collection.extend({
-    model: ContactManager.Contact,
+  Entities.ContactCollection = Backbone.Collection.extend({
+    model: Entities.Contact,
     comparator: function(a, b){
       var aFirstName = a.get("firstName");
       var bFirstName = b.get("fistName");
@@ -20,18 +20,41 @@ ContactManager.module("Entities", function(Entities, ContactManager, Backbone, M
           return 0;
         } else {
           if(aLastName < bLastName){
-            return -1;
-          } else {
             return 1;
+          } else {
+            return -1;
           }
         }
       } else {
         if(aFirstName < bFirstName){
-          return -1;
-        } else {
           return 1;
+        } else {
+          return -1;
         }
       }
     }
+  });
+
+  var contacts;
+
+  var initializeContacts = function(){
+    contacts = new Entities.ContactCollection([
+      {id: 1, firstName: "Alice", lastName: "Arten", phoneNumber: "555-0184"},
+      {id: 2, firstName: "Bob", lastName: "Brigham", phoneNumber: "555-0163"},
+      {id: 3, firstName: "Charlie", lastName: "Cambell", phoneNumber:"555-0129"}
+    ]);
+  };
+
+  var API = {
+    getContactEntities: function(){
+      if(contacts === undefined){
+        initializeContacts();
+      }
+      return contacts;
+    }
+  };
+
+  ContactManager.reqres.setHandler("contact:entities", function(){
+    return API.getContactEntities();
   });
 });
